a04d20dd68043d30c1b8c1cc185495b54465b8fc
VitorJusten <vitorjusten22@gmail.com>
2026-05-25 10:15:18 -0300

    Refactor

--- src/main/java/com/justen/infrastructure/AppProperties.java
+++ src/main/java/com/justen/infrastructure/AppProperties.java
@@ -1,6 +1,5 @@
 package com.justen.infrastructure;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
@@ -14,63 +13,8 @@ import lombok.Data;
  */
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "auth")
 public class AppProperties {
 
-    private String uiUrl;
-    
-    private String issuer = "http://localhost:8081";
-
-    private Cors cors = new Cors();
-
-    private Timer timer;
-
-    private Authorization authorization;
-
-    private Api api;
-
-    private Jwt jwt;
-
-    @Data
-    public static class Cors {
-        private java.util.List<String> allowedOrigins = java.util.List.of();
-    }
-
-    @Data
-    public static class Timer {
-
-        private Integer accessTokenTTL;
-
-        private Integer refreshTokenTTL;
-
-    }
-
-    @Data
-    public static class Authorization {
-
-        private String clientId;
-
-        private String clientSecret;
-
-        private String scope;
-
-    }
-
-    @Data
-    public static class Api {
-
-        private String clientId;
-
-        private String clientSecret;
-
-        private String scope;
-
-    }
-
-    @Data
-    public static class Jwt {
-        private String publicKey;
-        private String privateKey;
-    }
+   
 
 }
